c0776a90d7587b1853ec443d0025c003884a1866
Logan-D00255031 <145552065+Logan-D00255031@users.noreply.github.com>
2024-02-12 23:59:10 +0000

    Finished Comments for Q4

--- src/Q4.java
+++ src/Q4.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 
 public class Q4 {
     public static void main(String[] args) {
+        // Create string for HTML tags
         String htmlTags = "<p> <ul> <li> </li> </u> <a> </p>";
+        // Display the tags
         System.out.println(htmlTags);
+        // Create a scanner for the HTML tags
         Scanner in = new Scanner(htmlTags);
+        // Create Stack
         ArrayDeque<String> tags = new ArrayDeque<>();
 
         while (in.hasNext()) {
@@ -16,21 +20,27 @@ public class Q4 {
             //System.out.println(tag);
             // Check if it's a closing tag
             if(tag.charAt(0) == '/') {
+                // Look at current tag on top of the stack
                 String currentTag = tags.peek();
-                // Check if last tag is not the opening of the new tag
+                // Check if the closing tag is the same tag as the open tag of the current tag on top of the stack
                 if(tag.substring(1).equals(currentTag)) {
+                    // Remove the tag on top of the stack
                     tags.remove();
                 }
                 else {
+                    // Display error
                     System.out.println("The Tag '" + tag.substring(1) + "' is not the current open tag or does not exist");
                 }
-            }
+            } // Not a closing tag
             else {
+                // Push the tag to the stack
                 tags.push(tag);
             }
             //System.out.println(tags);
         }
+        // If the stack is not empty
         if(!tags.isEmpty()) {
+            // Display the open tags
             System.out.printf("Tag(s) not closed: " + tags);
         }
 
